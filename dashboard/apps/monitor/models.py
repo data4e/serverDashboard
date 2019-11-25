436806a1b32abab398afc2148046e52a51c68004
刘文龙 <1@gitor.org>
2019-11-25 10:09:22 +0800

    修改模型

--- dashboard/apps/monitor/models.py
+++ dashboard/apps/monitor/models.py
@@ -5,3 +5,21 @@ from common.models import BaseModel
 
 class Cpu(BaseModel):
     pass
+
+    class Meta:
+        db_table = 'sd_cpu'
+
+
+class ServerInfo(BaseModel):
+    # 服务器名称
+    name = models.CharField('服务器名', max_length=32)
+    desc = models.TextField('备注')
+    score = models.IntegerField('核心数')
+
+    class Meta:
+        db_table = 'sd_server_info'
+        verbose_name = '服务器信息'
+        verbose_name_plural = '服务器信息'
+
+    def __str__(self):
+        return self.name
